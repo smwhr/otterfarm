8fb26bb9c1ae874f0f67243fe3ea18afc06e3760
Ju/TroisYaourts <julien@troisyaourts.com>
2014-11-06 15:37:53 +0100

    plein de tests

--- src/OtterFarm/Otter.php
+++ src/OtterFarm/Otter.php
@@ -1,3 +1,17 @@
 <?php
 
-namespace SupInternet\OtterFarm;
+namespace OtterFarm;
+
+
+class Otter{
+
+  private $sex = null;
+
+  public function __construct($sex="m"){
+    $this->sex = $sex;
+  }
+
+  public function getSexe(){
+    return $this->sex;
+  }
+}
--- tests/OtterFarm/FarmTest.php
+++ tests/OtterFarm/FarmTest.php
@@ -2,6 +2,7 @@
 namespace OtterFarm\Tests;
 
 use OtterFarm\Farm;
+use OtterFarm\Otter;
 
 class FarmTest extends \PHPUnit_Framework_TestCase
 {
@@ -15,4 +16,95 @@ class FarmTest extends \PHPUnit_Framework_TestCase
       $this->assertEquals($expectedMessage, $result);
 
     }
+
+  /**
+   * @test
+   */
+  public function familyIsArray(){
+    $farm = new Farm();
+    $result = $farm->getFamily();
+    $this->assertInternalType('array', $result);
+  }
+  
+  /**
+   * @test
+   */
+  public function kidIsOtter(){
+    $o1 = new Otter("m");
+    $o2 = new Otter("f");
+    $f = new Farm();
+
+    $kid = $f->reproduce($o1, $o2);
+    $this->assertInstanceOf('OtterFarm\Otter', $kid);
+
+  }
+  /**
+   * @test
+   * @expectedException        \Exception
+   * @expectedExceptionMessage ONLR
+   */
+  public function unPapaUneMaman(){
+    $o1 = new Otter("m");
+    $o2 = new Otter("m");
+    $f = new Farm();
+
+    $this->setExpectedException('\Exception','ONLR');
+
+    $kid = $f->reproduce($o1, $o2);
+  }
+
+  /**
+   * @test
+   */
+  public function familyCountIncrease(){
+    $o1 = new Otter("m");
+    $o2 = new Otter("f");
+    $farm = new Farm([$o1, $o2]);
+    $farm->reproduce($o1,$o2);
+    $this->assertCount(3,$farm->family);
+  }
+
+  /**
+   * @test
+   */
+  public function failToutLeTemps(){
+      //$this->fail("Normal");
+  }
+
+  public function provideFamilies(){
+    return array(
+      [array()],
+      [array(new Otter("f"), new Otter("f"), new Otter("f"))],
+      [array(new Otter("f"), new Otter("f"), new Otter("m"))]
+      );
+  }
+
+  /**
+   * @test
+   * @dataProvider provideFamilies
+   */
+  public function toutLeMondeEstLa($liste){
+
+      $f = new Farm($liste);
+      $this->assertEquals(count($liste),$f->nbFamily() );
+
+  }
+
+  public function provideCouples(){
+    return [
+        [new Otter("f"), new Otter("m")],
+        [new Otter("f"), new Otter("f")],
+        [new Otter("m"), new Otter("m")],
+        [new Otter("m"), new Otter("f")]
+    ];
+  }
+
+  /**
+   * @test
+   * @dataProvider provideCouples
+   */
+  public function testRepro2($o1, $o2){
+    //do something to test all possible couples
+  }
+
 }
