39a6d9121a7dfa4e1636ce7115276ce831277f0d
Ju/TroisYaourts <julien@troisyaourts.com>
2014-11-06 16:29:14 +0100

    La flemme de l eleve

--- src/OtterFarm/Store.php
+++ src/OtterFarm/Store.php
@@ -1,18 +1,19 @@
 <?php
 
-namespace OtterFarm
+namespace OtterFarm;
 
 class Store{
+
   public function __construct(){
     
-    $this->url = "https://api-3t.sandbox.paypal.com/vp";
+    $this->url = "https://api-3t.sandbox.paypal.com/nvp";
 
     $this->email = "julienzamor-facilitator_api1.gmail.com";
     $this->password = "1370009121";
     $this->signature = "AsfDksnALQtfAi3TNnRo0-QTsFxvANeXM63Sg0BP955mX1XZAZlq0gKE";
   }
 
-  public function requestPayment(){
+  public function requestPayment($amount){
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL,$this->url);
     curl_setopt($ch, CURLOPT_POST, 1);
@@ -23,17 +24,23 @@ class Store{
             "PWD" =>  $this->password,
             "SIGNATURE" => $this->signature,
             "VERSION" => 78,
-            "AMT" => 10,
+            "AMT" => $amount,
             "returnUrl" => "http://myotterfarm.com",
             "cancelUrl"  => "http://myotterfarm.com"
             );
+
     curl_setopt($ch, CURLOPT_POSTFIELDS, 
           http_build_query( $post_var ));
-
+    
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $server_output = curl_exec ($ch);
+    $server_output = curl_exec($ch);
+
+    if(empty($server_output)){
+      throw new \Exception(curl_error($ch));
+    }
 
     curl_close ($ch);
+    return $server_output;
   }
 
 }
--- web/buy.php
+++ web/buy.php
@@ -1,4 +1,12 @@
 <?php
 include(__DIR__."/../vendor/autoload.php");
 
+$qty = $_POST['qty'];
+$price = $_POST['price'];
+
+$amount = $qty*$price;
+
 $s = new OtterFarm\Store;
+$response = $s->requestPayment($amount);
+
+echo $response;
